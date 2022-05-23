91364df2f518666942fdc38fb1891bd9da412e7f
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2022-05-22 21:57:42 -0400

    Generate teeny code on backend

--- pages/api/[teeny_code].ts
+++ pages/api/[teeny_code].ts
@@ -5,20 +5,19 @@ import { hasNone } from "../../utils";
 import { supabase } from "../../libs/subabase";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-	const { emojis } = req.query;
-	const { data: teenyUrlData, error } = await supabase.from("urls").select().match({ emojis });
+	const { teeny_code } = req.query;
+	const { data: teenyUrlData, error } = await supabase.from("urls").select().match({ teeny_code });
 
 	console.dir({ data: teenyUrlData, error });
 
 	if (error || hasNone(teenyUrlData)) {
 		res.redirect(ROOT_URL);
 	} else {
-		const { data, error } = await supabase
+		const { error } = await supabase
 			.from("urls")
 			.update({ hits: Number(teenyUrlData[0]?.hits) + 1 })
-			.match({ emojis });
+			.match({ teeny_code });
 
-		console.log("data:", data);
 		if (error) console.error(error);
 		res.redirect(`/redirect?to=${teenyUrlData[0].long_url}`);
 	}
