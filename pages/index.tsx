0c010e99c8ca2a5ae5fed2b59ad5c705d7e0cb83
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2022-06-12 22:22:14 -0400

    Add component directory, begin adding styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
-import { getTopEmojis, isValidUrl, log, removeTrailingSlash } from "@utils";
+import { Logo, UrlForm } from "components";
 
 import Head from "next/head";
-import Link from "next/link";
 import React from "react";
 import { TopEmoji } from "../types";
 import { User } from "@supabase/supabase-js";
+import { getTopEmojis } from "@utils";
 import { supabase } from "@libs/subabase";
 
 type IndexPageProps = {
@@ -16,54 +16,28 @@ const Home = ({
 	// urls,
 	topEmojis,
 }: IndexPageProps) => {
-	const [longUrl, setLongUrl] = React.useState<string>("");
 	const [authedUser, setAuthedUser] = React.useState<User | null>(null);
 
-	const isSet = (values: string | string[]) => {
-		if (Array.isArray(values)) return values.every((value: string) => value.length > 0);
-		return values.length > 0;
-	};
+	// const handleLogout = async (e: React.FormEvent) => {
+	// 	e.preventDefault();
 
-	const handleUpdateLongUrl = (e: React.ChangeEvent<HTMLInputElement>) => setLongUrl(e.target.value);
+	// 	const { error } = await supabase.auth.signOut();
 
-	const handleCreateTeenyLink = async () => {
-		if (!isSet(longUrl)) return alert("Required fields not set");
-		if (!isValidUrl(longUrl)) return alert("Not a valid URL");
-
-		const queryString = new URLSearchParams({
-			long_url: String(removeTrailingSlash(longUrl)),
-		});
-
-		const response = await fetch(`/api/createTeenyUrl?${queryString}`);
-		const { data, error } = await response.json();
-
-		if (!error) {
-			setLongUrl("");
-		}
-
-		log({ data, error });
-	};
-
-	const handleSubmit = async (e: React.FormEvent) => {
-		e.preventDefault();
-
-		const { user, session, error } = await supabase.auth.signIn({
-			provider: "github",
-		});
-	};
-
-	const handleLogout = async (e: React.FormEvent) => {
-		e.preventDefault();
-
-		const { error } = await supabase.auth.signOut();
-
-		if (error) console.error(error);
-	};
+	// 	if (error) console.error(error);
+	// };
 
 	supabase.auth.onAuthStateChange((_, session) => {
 		const user = session?.user || null;
 		setAuthedUser(user);
 	});
+
+	// const handleSubmit = async (e: React.FormEvent) => {
+	// 	e.preventDefault();
+
+	// 	const { user, session, error } = await supabase.auth.signIn({
+	// 		provider: "github",
+	// 	});
+	// };
 
 	return (
 		<div className="app">
@@ -73,9 +47,18 @@ const Home = ({
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
-			<main>
-				<h1>Welcome to teeny.fun</h1>
-				{!authedUser ? (
+			<main className="main">
+				<div className="home-page">
+					<h1>
+						<span aria-hidden={true} hidden={true}>
+							teeny.fun
+						</span>
+						<span>
+							<Logo size="20rem" />
+						</span>
+					</h1>
+
+					{/* {!authedUser ? (
 					<form onSubmit={handleSubmit}>
 						<button type="submit">Login with GitHub</button>
 					</form>
@@ -86,23 +69,15 @@ const Home = ({
 							<button type="submit">Logout</button>
 						</form>
 					</>
-				)}
-				<div className="form">
-					<label htmlFor="long-url">
-						URL
-						<input type="text" id="long-url" required value={longUrl} onChange={handleUpdateLongUrl} />
-					</label>
-					<button type="submit" onClick={handleCreateTeenyLink}>
-						Create Teeny Link
-					</button>
-				</div>
-				<p>
-					<Link href="/">Login</Link> to Customize your teeny code.
+				)} */}
+					<UrlForm />
+					{/* <p>
+					<Link href="/">Sign up</Link> to update your links.
 				</p>
 				<p>
 					<Link href="/">Already have an account?</Link>
-				</p>
-				{topEmojis && (
+				</p> */}
+					{/* {topEmojis && (
 					<table>
 						<thead>
 							<tr>
@@ -122,7 +97,8 @@ const Home = ({
 								.slice(0, 10)}
 						</tbody>
 					</table>
-				)}
+				)} */}
+				</div>
 			</main>
 
 			<footer>
